9c2dfa09af3bae203ea659298f813cc022f4c1c9
Mario Hernandez <marioxen.secure@gmail.com>
2024-08-07 13:14:58 -0600

    Incremento de solicitudes, propiedad Next

--- src/components/api.js
+++ src/components/api.js
@@ -5,22 +5,40 @@ const BASE_URL = 'https://sandbox.belvo.com/api'; // Reemplaza con tu URL base
 const API_TOKEN = '848db9af-c89a-40e9-afcd-c599b438a65e:DqgFcXMIQ7Z7cBu#Po#6yUgD7aqn9y_NlAulP_X6hhyk9rAw1EtAj_NF3yYp@Nf2'; // Reemplaza con tu token
 
 export const fetchTransactions = async () => {
-  try {
-    const config = {
-      method: 'get',
-      url: `${BASE_URL}/transactions/`, // Cambiado a 'transactions/' para la solicitud requerida
-      params: {
-        page: 1,
-        link: "61471984-bf6d-43c6-a5d9-08364ed86ede"
-      },
-      headers: {
-        Authorization: `Basic ${btoa(API_TOKEN)}` // Codifica el token en base64 para Basic Auth
-      }
-    };
+  let transactions = [];
+  let nextPageUrl = `${BASE_URL}/transactions/`; // URL inicial de la API
+  let page = 1; // Comenzamos con la primera página
+  let requestCount = 0; // Contador de solicitudes
 
-    const response = await axios.request(config);
-    //console.log('Fetched transactions:', response.data);
-    return response.data;
+  try {
+    while (nextPageUrl && requestCount < 10) { // Añadido límite de solicitudes
+      const config = {
+        method: 'get',
+        url: nextPageUrl,
+        params: {
+          page: page,
+          link: "61471984-bf6d-43c6-a5d9-08364ed86ede" // Reemplaza con el valor adecuado si es necesario
+        },
+        headers: {
+          Authorization: `Basic ${btoa(API_TOKEN)}` // Codifica el token en base64 para Basic Auth
+        }
+      };
+
+      const response = await axios.request(config);
+      transactions = transactions.concat(response.data.results || response.data.transactions);
+
+      // Actualiza la URL para la siguiente página
+      nextPageUrl = response.data.next || null;
+      
+      // Incrementa el número de la página
+      page++;
+      
+      // Incrementa el contador de solicitudes
+      requestCount++;
+    }
+
+    console.log('Fetched transactions:', transactions);
+    return transactions;
   } catch (error) {
     console.error('Error fetching transactions:', error);
     throw error;
